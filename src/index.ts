7511ffbb046243b7b16c156db9b7d3a7a9ba7779
J.D. Shaeffer <shaefferjd@gmail.com>
2023-07-15 19:20:25 -0400

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from 'express';
+import puppeteer from 'puppeteer';
 
 (async () => {
   const app = express();
   app.get('/', (req: Request, res: Response) => {
     res.send('hello!');
   });
+
+  const browser = await puppeteer.launch({
+    headless: 'new',
+  });
+  const page = await browser.newPage();
+  await page.goto('https://bikepacking.com/events');
+
+  const events = await page.evaluate(() =>
+    // Array.from(document.querySelectorAll('div[id^="post"]'), (e) => )
+    document.querySelectorAll('div[id^="post"]'),
+  );
+
+  await browser.close();
+  console.log(events);
 
   const port = 9000;
   app.listen(port, () => {
