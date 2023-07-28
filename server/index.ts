2f3808601d5e4a605155ec3fc06b7835df9f8551
J.D. Shaeffer <shaefferjd@gmail.com>
2023-07-27 20:57:07 -0400

    server

---
+++ server/index.ts
@@ -0,0 +1,60 @@
+import puppeteer from 'puppeteer';
+import cors from 'cors';
+import * as cheerio from 'cheerio';
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+);
+
+app.get('/api/events', async (req, res) => {
+  const getBikepackingEvents = async () => {
+    const browser = await puppeteer.launch({
+      headless: 'new',
+    });
+    const page = await browser.newPage();
+    await page.goto('https://bikepacking.com/events');
+
+    const eventsHTML = await page.evaluate(() =>
+      Array.from(
+        document.querySelectorAll('div[id^="post-"]'),
+        (e) => e.innerHTML
+      )
+    );
+
+    const events = eventsHTML.map((event) => {
+      const $ = cheerio.load(event);
+      const dateAndPrice = $('div.event-list-when');
+      const [category, location] = $('div.event-list-where')
+        .text()
+        .split(' / ');
+      return {
+        title: $('h2').text(),
+        eventUrl: $('a').attr('href'),
+        imgSrc: $('img').attr('src'),
+        date: dateAndPrice.find('span.tribe-event-date-start').text(),
+        price: dateAndPrice.find('span:not(.tribe-event-date-start)').text(),
+        category,
+        location,
+        detail: $('p').text(),
+      };
+    });
+
+    return events;
+  };
+
+  console.log('getting events...');
+  const events = await getBikepackingEvents();
+  res.json(events);
+  console.log('events sent');
+});
+
+app.listen(port, () => {
+  console.log('running server on 8000');
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+interface BikepackingEvent {
+  title: string;
+  eventUrl: string;
+  imgSrc: string;
+  date: string;
+  price: string;
+  category: string;
+  location: string;
+  detail: string;
+}
+
 function App() {
+  const [events, setEvents] = useState<BikepackingEvent[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const res = await fetch('http://localhost:8000/api/events');
+      const eventsJson = await res.json();
+      setEvents(eventsJson);
+      setLoading(false);
+    })();
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>test test test</h1>
+    <div className='app'>
+      <div className='app-header'>
+        <h1>bikepacking events</h1>
+      </div>
+      <div className='events'>
+        {loading ? (
+          <p>loading events from bikepacking.com ...</p>
+        ) : events ? (
+          events.map((event) => <p>{event.title}</p>)
+        ) : (
+          <p>none</p>
+        )}
+      </div>
     </div>
   );
 }
