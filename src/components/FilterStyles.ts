fa920abf66ce0aa77c7397376b45ff8090432f82
JD Shaeffer <shaefferjd@gmail.com>
2023-10-02 18:33:25 -0700

    mobile styles

--- src/components/FilterStyles.ts
+++ src/components/FilterStyles.ts
@@ -1,13 +1,15 @@
 import { StylesConfig } from 'react-select';
 
 const filterStyles: StylesConfig = {
-  control: (base) => ({
-    ...base,
-    backgroundColor: '#282c34',
-    border: '2px solid white',
-    color: 'white',
-    width: '15vw',
-  }),
+  control: (base) => {
+    return {
+      ...base,
+      backgroundColor: '#282c34',
+      border: '2px solid white',
+      color: 'white',
+      width: window.screen.width > 700 ? '15vw' : '80vw',
+    };
+  },
   menu: (base) => ({
     ...base,
     backgroundColor: '#282c34',
